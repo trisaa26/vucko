06047d290b59e5e1a6b4551c19bc882411a51939
trisaa26 <stefantrisa9999@gmail.com>
2023-11-14 14:26:28 +0100

    New Feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,14 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function () {
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+const menuLinks = document.querySelectorAll(".navbar-collapse a");
+const navbarToggler = document.querySelector(".navbar-toggler");
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+    navbarCollapse.classList.remove("show");
+    navbarToggler.setAttribute("aria-expanded", "false");
+  });
+});
